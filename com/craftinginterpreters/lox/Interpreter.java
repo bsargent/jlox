793a75fe721ad7d5c4d597ef487a949fe8714317
Brian Sargent <sargent.brianj@gmail.com>
2023-04-05 18:47:15 -0600

    Complete chapter 7 code

--- com/craftinginterpreters/lox/Interpreter.java
+++ com/craftinginterpreters/lox/Interpreter.java
@@ -2,6 +2,15 @@ package com.craftinginterpreters.lox;
 
 class Interpreter implements Expr.Visitor<Object> {
 
+    void interpret(Expr expression) { 
+        try {
+          Object value = evaluate(expression);
+          System.out.println(stringify(value));
+        } catch (RuntimeError error) {
+          Lox.runtimeError(error);
+        }
+      }
+
     @Override
     public Object visitLiteralExpr(Expr.Literal expr) {
         return expr.value;
@@ -22,6 +31,7 @@ class Interpreter implements Expr.Visitor<Object> {
 
         switch (expr.operator.type) {
             case MINUS:
+                checkNumberOperand(expr.operator, right);
                 return -(double) right;
             case BANG:
                 return !isTruthy(right);
@@ -58,7 +68,7 @@ class Interpreter implements Expr.Visitor<Object> {
                 checkNumberOperands(expr.operator, left, right);
                 return (double) left <= (double) right;
             case MINUS:
-                checkNumberOperand(expr.operator, right);
+                checkNumberOperands(expr.operator, left, right);
                 return (double) left - (double) right;
             case PLUS:
                 if (left instanceof Double && right instanceof Double) {
@@ -106,4 +116,18 @@ class Interpreter implements Expr.Visitor<Object> {
 
         return a.equals(b);
     }
+    private String stringify(Object object) {
+        if (object == null) return "nil";
+    
+        if (object instanceof Double) {
+          String text = object.toString();
+          if (text.endsWith(".0")) {
+            text = text.substring(0, text.length() - 2);
+          }
+          return text;
+        }
+    
+        return object.toString();
+      }
+
 }
